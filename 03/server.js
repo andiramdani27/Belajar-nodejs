e9ff95db55e59850927bc7db99a0f655f9016a50
Andi Ramdani <andi8337@gmail.com>
2018-12-27 16:13:13 +0700

    coba res params

--- 03/server.js
+++ 03/server.js
@@ -5,6 +5,11 @@ app.get('/', function(request, response){
     response.send('hallo ini dari andi ramdani');
 })
 
+app.get('/users/:name', function(request, response){
+    response.send("Namanya adalah : " + request.params.name);
+})
+
+
 app.listen(3000);
 
 console.log("server running on http://localhost:3000");
